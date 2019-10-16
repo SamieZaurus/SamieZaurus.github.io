3f7c2e02e5437b14a29fae2cc651bf9f7d268e2d
SamieZaurus <samuelmiddendorp@hotmail.com>
2019-10-16 13:53:33 +0200

    Latest post

--- v2/scripts/script.js
+++ v2/scripts/script.js
@@ -3,7 +3,7 @@
 let navToggled = false;
 
 
-alert(window.innerHeight);
+//alert(window.innerHeight);
 function toggleNav(){
     console.log("debug");
     let nav  = document.getElementById('nav');
@@ -19,7 +19,32 @@ function toggleNav(){
         navToggled = true;
         return;
     }  
-    nav.style.width = '25%';
+    nav.style.width = '100%';
+    mainContent.style.width = '75%';   
+    toggler.style.left = '25%';
+    toggler_arrow.style.transform = 'rotate(0deg)'
+    navToggled = false;
+
+    return;
+
+
+}
+function toggleNavMobile(){
+    console.log("debug");
+    let nav  = document.getElementById('nav');
+    let mainContent = document.getElementById('main-content');
+    let toggler = document.getElementById('nav-toggler-mobile');
+    let toggler_arrow = document.getElementById('toggler-arrow-mobile');
+
+    if(!navToggled){
+        nav.style.width = '0%';
+        mainContent.style.width = '100%';  
+        toggler.style.left = '0px'
+        toggler_arrow.style.transform = 'rotate(180deg)'
+        navToggled = true;
+        return;
+    }  
+    nav.style.width = '100%';
     mainContent.style.width = '75%';   
     toggler.style.left = '25%';
     toggler_arrow.style.transform = 'rotate(0deg)'
